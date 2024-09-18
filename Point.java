3c0e3e41de072023420de65ebb9157f3aeb26c62
EfeSelamet <164161954+EfeSelamet@users.noreply.github.com>
2024-09-18 13:42:52 +0300

    Add files via upload

---
+++ Point.java
@@ -0,0 +1,10 @@
+public class Point {
+    public double x;
+    public double y;
+    public int number;
+    public Point(double x, double y, int number) {
+        this.x = x;
+        this.y = y;
+        this.number = number;
+
+    }
